e07978b37a2a89f2e35faa800579abfe24b5bc7f
Zac Smith <zrs3141592@gmail.com>
2020-07-27 13:13:15 -0600

    basic scaffold

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+const homeRouter = require('./routes/home.js')
+
+const app = express()
+app.use(helmet())
+app.use(morgan('dev'))
+app.set('views', path.join(__dirname), 'views')
+app.set('view engine', 'hjs')
+
+app.use('/', homeRouter)
+
+const PORT = 5775
+app.listen(PORT, () => {
+  console.log(`\n** Server is listening at http://localhost:${PORT} **\n`)
+})
