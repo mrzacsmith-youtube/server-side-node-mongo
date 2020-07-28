03a437b1259348c50cf5bcf2a9fe82cf34abcd17
Zac Smith <zrs3141592@gmail.com>
2020-07-27 22:43:50 -0600

    fix register error

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,21 @@
 const router = require('express').Router()
+const User = require('../models/User.js')
 
 router.post('/', (req, res) => {
+  User.create(req.body, (err, user) => {
+    if (err) {
+      res.json({
+        confirmation: 'failed',
+        error: err,
+      })
+      return
+    }
+  })
+
   res.json({
-    data: req.body,
+    confirmation: 'success',
+    // data: req.body,
+    user: req.body,
   })
 })
 
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const helmet = require('helmet')
 const morgan = require('morgan')
 
 const homeRouter = require('./routes/home.js')
 const registerRouter = require('./routes/register.js')
+
+mongoose.connect(
+  'mongodb://localhost/serverSide',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, data) => {
+    if (err) {
+      console.log('DB connection failed')
+      return
+    }
+    console.log('DB connection successful')
+  }
+)
 
 const app = express()
 app.use(helmet())
@@ -17,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', homeRouter)
 app.use('/register', registerRouter)
 
-const PORT = 5775
+const PORT = 5995
 app.listen(PORT, () => {
   console.log(`\n** Server is listening at http://localhost:${PORT} **\n`)
 })
