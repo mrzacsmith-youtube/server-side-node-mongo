7c5661c7490e07ada4a2bf36c6ac2791783bea52
Zac Smith <zrs3141592@gmail.com>
2020-07-29 19:47:07 -0600

    fix error handle route

--- routes/login.js
+++ routes/login.js
@@ -5,32 +5,17 @@ const User = require('../models/User.js')
 
 router.post('/', (req, res, next) => {
   const email = req.body.email
-  User.find({ email: email }, (err, users) => {
+  User.findOne({ email: email }, (err, user) => {
     if (err) {
-      // res.json({
-      //   confirmation: 'failed',
-      //   error: err,
-      // })
       return next(err)
     }
-
-    if (users.length === 0) {
-      // res.json({
-      //   confirmation: 'failed',
-      //   message: 'User and/or password is incorrect!',
-      // })
+    if (user == null) {
       return next(new Error('User and/or password is incorrect.'))
     }
-
-    const user = users[0]
 
     // check password
 
     if (user.password != req.body.password) {
-      // res.json({
-      //   confirmation: 'failed',
-      //   message: 'User and/or password is incorrect.',
-      // })
       return next(new Error('User and/or password is incorrect.'))
     }
 
