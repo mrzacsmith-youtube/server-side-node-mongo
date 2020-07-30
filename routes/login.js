7bd30b388f156257afc539e75c147f8891aaa5c6
Zac Smith <zrs3141592@gmail.com>
2020-07-29 20:48:58 -0600

    code clean up

--- routes/login.js
+++ routes/login.js
@@ -10,28 +10,4 @@ router.post(
   })
 )
 
-// router.post('/', (req, res, next) => {
-//   const email = req.body.email
-//   User.findOne({ email: email }, (err, user) => {
-//     if (err) {
-//       return next(err)
-//     }
-
-//     // check user = null, then send to error handler
-//     if (user == null) {
-//       return next(new Error('User and/or password is incorrect.'))
-//     }
-
-//     // check password
-//     if (user.password != req.body.password) {
-//       return next(new Error('User and/or password is incorrect.'))
-//     }
-
-//     res.json({
-//       confirmation: 'success',
-//       user: user,
-//     })
-//   })
-// })
-
 module.exports = router
