dbaa64a280209065e71004a3aa71e1b3214c3dc5
Zac Smith <zrs3141592@gmail.com>
2020-07-29 22:37:04 -0600

    create basic items
    Uses for loop in hjs to render hardcoded data. Next will be to create model and db collection

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,13 @@
 const router = require('express').Router()
 
+const items = [
+  { name: 'Item 1', description: '', price: 10 },
+  { name: 'Item 2', description: '', price: 20 },
+  { name: 'Item 3', description: '', price: 15 },
+  { name: 'Item 4', description: '', price: 9 },
+  { name: 'Item 5', description: '', price: 44 },
+]
+
 router.get('/', (req, res, next) => {
   const user = req.user
 
@@ -10,6 +18,7 @@ router.get('/', (req, res, next) => {
 
   const data = {
     user: user,
+    items: items,
   }
 
   res.render('account', data)
